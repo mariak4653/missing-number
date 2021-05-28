b7d9b6f7f38c440ebc31a06649ab7c12413e0f25
mariak4653 <84856920+mariak4653@users.noreply.github.com>
2021-05-28 11:09:37 -0400

    Update chiffreManquant.java

--- chiffreManquant.java
+++ chiffreManquant.java
@@ -2,36 +2,36 @@
 //
 //Maria Kobilaski
 //5/11/2021
-//Algorithme chiffre array 
+//Algorithm missing number 
 //
 //////////////////
 import java.util.Scanner;//import scanner
 public class chiffreManquant {
     public static void main(String args[]) {
-      int [] array = new int [10]; //array de 10 cases
+      int [] array = new int [10]; //array of 10
       
-      //pour calculer les somes
+      //to calculate the sums
       int attendue=0;
       int actuel=0;
       
       Scanner cat = new Scanner (System.in);
-      for(int i=0; i<array.length; i++){ //inserer les valeurs dans l'array
+      for(int i=0; i<array.length; i++){ //insert values into array
         System.out.print("nombre " + (i+1) +" = ");
         array[i]= cat.nextInt();
       }
-      for(int j=0; j<array.length; j++){ //va calculer le somme actuel de 1 a 10
+      for(int j=0; j<array.length; j++){ //calculate the actual sum of numbers in array
             actuel = actuel + array[j];
         }
       
-      for(int j=0; j<=10; j++){ //va calculer le somme attendue de 1 a 10
+      for(int j=0; j<=10; j++){ //calculate the expected sum of numbers from 1-10
             attendue = attendue + j;
       }
       
-      if(attendue-actuel==0){//si il n'y a pas de zero
+      if(attendue-actuel==0){//if there isn't a zero
           System.out.println("Il n'y a pas de chiffre manquant");
       }
-      else{//quand il y a un zero
-          System.out.println("Les chiffre manquant est " +(attendue-actuel));//calculer le difference
+      else{//when there is a zero
+          System.out.println("Les chiffre manquant est " +(attendue-actuel));//calculate the difference to find and print the missing number
       }
       
     }
